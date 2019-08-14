a1521a4720951822447badecb3dc3b8708a4280c
Administrator <1298187046@qq.com>
2019-08-14 21:18:10 +0800

    hey

--- PO/base_driver/base_yaml.py
+++ PO/base_driver/base_yaml.py
@@ -1,6 +1,15 @@
 import yaml
+import os
+
+path = os.path.dirname(os.path.dirname(__file__))
+print(path)
 
 
-def find_yaml_data(path):
-    with open('./data/%s.yml' % path, 'rb') as f:
-        return yaml.load(f)
+def find_yaml_data(filename, key):
+    with open(path + '/data/%s.yml' % filename, 'rb') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)[key]
+        for i in data.values():
+            yield i
+
+
+find_yaml_data('yaml_data', 'data')
--- PO/scripts/test_search.py
+++ PO/scripts/test_search.py
@@ -1,4 +1,4 @@
-import os,sys
+import os, sys
 
 import pytest
 
@@ -8,20 +8,13 @@ from PO.base_driver.dirver_init import base_driver
 from PO.base_driver.base_yaml import find_yaml_data
 
 
-def data_with_key(key):
-    return find_yaml_data('yaml_data')[key]
-
 class Test_Search(object):
 
-    def setup(self):
-        self.driver = base_driver()
-        self.page=Search_Page(self.driver)
+    # def setup(self):
+    #     self.driver = base_driver()
+    #     self.page = Search_Page(self.driver)
 
-    @pytest.mark.parametrize('keys',data_with_key('data'))
-    def test_click(self,keys):
-        self.page.input_search()
-        self.page.send_text(text=keys)
-
-
-
-
+    @pytest.mark.parametrize('args', find_yaml_data('yaml_data', 'data'))
+    def test_click(self,args):
+        # self.page.input_search()
+        print(args['username'],args['password'])
